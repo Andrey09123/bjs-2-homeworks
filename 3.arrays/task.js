5c0f234eece8cc2a3d425ae939ad979bf2874083
Andre Arkhipov <andre.arhipow@gmail.com>
2022-10-05 20:08:02 +0300

    first commit

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -2,7 +2,7 @@ function compareArrays(arr1, arr2) {
   let result;
 
   // Ваш код
-
+  return (arr1.length === arr2.length) && (arr1.every((val,idx) => val === arr2[idx]));
   return result; // boolean
 }
 
@@ -10,6 +10,6 @@ function advancedFilter(arr) {
   let resultArr;
 
   // Ваш код
-
+  return arr.filter(val => val > 0).filter(val => val % 3 === 0).map(val => val * 10);
   return resultArr; // array
 }
