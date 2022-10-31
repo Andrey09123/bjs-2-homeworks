511462d1b36df455283dce52acf452e0be3e8492
Andre Arkhipov <andre.arhipow@gmail.com>
2022-10-31 20:58:13 +0300

    first commit

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -1,8 +1,42 @@
 function cachingDecoratorNew(func) {
-  // Ваш код
-}
+  let cache = []; 
+  function wrapper(...args) {
+      const hash = args.join(','); 
+      let objectInCache = cache.find((item) => hash in item); 
+      if (objectInCache ) { 
+          console.log("Из кэша: " + objectInCache[hash]);
+          return "Из кэша: " + objectInCache[hash];
+      }  
+      let result = func(...args); 
+      cache.push({[hash]:result}); 
+      console.log(cache);
+      if (cache.length > 5) { 
+        cache.shift(); 
+      }
+      console.log("Вычисляем: " + result);
+      return "Вычисляем: " + result;  
+  }
+  console.log(cache);
+  return wrapper;
+  }
 
 
-function debounceDecoratorNew(func) {
-  // Ваш код
-}
+
+  function debounceDecoratorNew(func, delay) {
+    let timeoutId;
+    return function wrapper(...args) {
+      if (timeoutId === undefined) {
+        wrapper.count = 1;
+        wrapper.allCount = 0;
+        func(...args);
+      }
+      if (timeoutId !== null) {
+        clearTimeout(timeoutId);      
+      }
+      timeoutId = setTimeout(() => {
+        func(...args);
+        wrapper.count++
+      }, delay);
+      wrapper.allCount++;
+    }
+  }
